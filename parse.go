5c3ad056a471b688f6d60598e2a92df06a53fc5b
Blake Mizerany <blake.mizerany@gmail.com>
2012-11-17 17:44:17 -0800

    break out parser

--- parse.go
+++ parse.go
@@ -10,66 +10,82 @@ var (
 	ErrUnexpectedEOF   = errors.New("unexpected EOF")
 
 	errPhase = errors.New("logfmt decoder out of sync - data changing underfoot?")
+	eof = errors.New("EOF")
 )
 
+type parser struct {
+	s *scanner
+}
+
+func (p *parser) next() (key string, val interface{}, err error) {
+	tok, err := p.s.nextT()
+	if err != nil {
+		return "", "", err
+	}
+	switch tok.t {
+	case tString:
+		s, ok := unquoteBytes(tok.src)
+		if !ok {
+			return "", "", errPhase
+		}
+		key = string(s)
+	case tIdent:
+		key = string(tok.src)
+	case tEOF:
+		return "", "", eof
+	default:
+		return "", "", ErrUnexpectedToken
+	}
+
+	tok, err = p.s.nextT()
+	if err != nil {
+		return "", "", err
+	}
+	switch tok.t {
+	case tEqual:
+	case tEOF:
+		return "", "", ErrUnexpectedEOF
+	default:
+		return "", "", ErrUnexpectedToken
+	}
+
+
+	tok, err = p.s.nextT()
+	if err != nil {
+		return "", "", err
+	}
+	switch tok.t {
+	case tString:
+		s, ok := unquoteBytes(tok.src)
+		if !ok {
+			return "", "", errPhase
+		}
+		val = string(s)
+	case tNumber:
+		// We don't need to worry about an error. We know it's a number.
+		val, _ = strconv.Atoi(string(tok.src))
+	case tIdent:
+		val = string(tok.src)
+	case tEOF:
+		return "", "", ErrUnexpectedEOF
+	default:
+		return "", "", ErrUnexpectedToken
+	}
+
+	return key, val, nil
+}
+
 func Unmarshal(b []byte, x map[string]interface{}) error {
-	s := newScanner(b)
+	p := &parser{newScanner(b)}
 	for {
-		var key string
-
-		tok, err := s.nextT()
+		key, val, err := p.next()
 		if err != nil {
-			return err
-		}
-		switch tok.t {
-		case tString:
-			s, ok := unquoteBytes(tok.src)
-			if !ok {
-				return errPhase
+			if err == eof {
+				return nil
 			}
-			key = string(s)
-		case tIdent:
-			key = string(tok.src)
-		case tEOF:
-			return nil
-		default:
-			return ErrUnexpectedToken
-		}
-
-		tok, err = s.nextT()
-		if err != nil {
 			return err
 		}
-		switch tok.t {
-		case tEqual:
-		case tEOF:
-			return ErrUnexpectedEOF
-		default:
-			return ErrUnexpectedToken
-		}
-
-
-		tok, err = s.nextT()
-		if err != nil {
-			return err
-		}
-		switch tok.t {
-		case tString:
-			s, ok := unquoteBytes(tok.src)
-			if !ok {
-				return errPhase
-			}
-			x[key] = string(s)
-		case tNumber:
-			// We don't need to worry about an error. We know it's a number.
-			x[key], _ = strconv.Atoi(string(tok.src))
-		case tIdent:
-			x[key] = string(tok.src)
-		case tEOF:
-			return ErrUnexpectedEOF
-		default:
-			return ErrUnexpectedToken
-		}
+		x[key] = val
 	}
 	return nil
 }
