497dc52c3333b6b32ab0329d5c6890d16e8dd30a
Blake Mizerany <blake.mizerany@gmail.com>
2012-11-19 18:42:34 -0800

    conver tests to table driven

--- convert_test.go
+++ convert_test.go
@@ -38,27 +38,27 @@ func convertAssign(dv reflect.Value, tok *token) error {
 	return nil
 }
 
-type convertTester struct {
-	t *testing.T
-}
-
-func (ct *convertTester) converts(dst reflect.Value, src *token, w interface{}) {
-	if err := convertAssign(dst, src); err != nil {
-		ct.t.Error(err)
-	}
-	idst := dst.Interface()
-	if !reflect.DeepEqual(idst, w) {
-		ct.t.Errorf("want %#v, got %#v", w, idst)
-	}
-}
-
 func TestConvert(t *testing.T) {
-	ct := &convertTester{t}
+	sv := reflect.Indirect(reflect.New(reflect.TypeOf("")))
+	tests := []struct{
+		v reflect.Value
+		t *token
+		w interface{}
+	}{
+		{sv, &token{tString, []byte("foo")}, "foo"},
+		{sv, &token{tIdent, []byte("true")}, "true"},
+		{sv, &token{tIdent, []byte("false")}, "false"},
+		{sv, &token{tNumber, []byte("1")}, "1"},
+		{sv, &token{tIdent, []byte("null")}, ""},
+	}
 
-	v := reflect.Indirect(reflect.New(reflect.TypeOf("")))
-	ct.converts(v, &token{tString, []byte("foo")}, "foo")
-	ct.converts(v, &token{tIdent, []byte("true")}, "true")
-	ct.converts(v, &token{tIdent, []byte("false")}, "false")
-	ct.converts(v, &token{tNumber, []byte("1")}, "1")
-	ct.converts(v, &token{tIdent, []byte("null")}, "")
+	for _, test := range tests {
+		if err := convertAssign(test.v, test.t); err != nil {
+			t.Error(err)
+		}
+		iv := test.v.Interface()
+		if !reflect.DeepEqual(iv, test.w) {
+			t.Errorf("want %#v, got %#v", test.w, iv)
+		}
+	}
 }
