6ea2dba601ef7836bb7da0d4dc1947951311facf
Keith Rarick <kr@xph.us>
2012-11-19 21:16:30 -0800

    remove dead code

--- token.go
+++ token.go
@@ -22,22 +22,6 @@ type token struct {
 	src string
 }
 
-func (tok *token) isEOF() bool {
-	return tok.t == tEOF
-}
-
-func (tok *token) isKey() bool {
-	return tok.t == tString || tok.t == tIdent
-}
-
-func (tok *token) isVal() bool {
-	return tok.t == tString || tok.t == tIdent || tok.t == tNumber
-}
-
-func (tok *token) isEqual() bool {
-	return tok.t == tEqual
-}
-
 func (tok *token) isNull() bool {
 	return tok.t == tIdent && tok.src == "null"
 }
