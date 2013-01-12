50255e70d96e0c71a0169ae58ec55db76481b4a1
Blake Mizerany <blake.mizerany@gmail.com>
2013-01-11 16:47:38 -0800

    ...

--- scanner.go
+++ scanner.go
@@ -41,6 +41,27 @@ func stateBeginKey(s *scanner, r rune) int {
 	return scanError
 }
 
+func stateBeginValue(s *scanner, r rune) int {
+	switch {
+	case unicode.IsLetter(r):
+		s.step = stateInIdent
+		s.next = stateBeginKey
+		return scanBeginValue
+	case unicode.IsDigit(r):
+		s.step = stateInNumberOrUnit
+		s.next = stateBeginValue
+		return scanBeginValue
+	case '"' == r:
+		s.step = stateInString
+		s.next = stateEqual
+		return scanBeginValue
+	case ' ' == r:
+		s.step = stateBeginValue
+		return scanSkipSpace
+	}
+	return scanError
+}
+
 func stateInIdent(s *scanner, r rune) int {
 	switch {
 	case unicode.IsLetter(r), unicode.IsDigit(r):
@@ -51,21 +72,36 @@ func stateInIdent(s *scanner, r rune) int {
 }
 
 func stateInString(s *scanner, r rune) int {
-	switch {
+	switch r {
 	case '\\':
 		s.step = stateInStringEsc
 		return scanContinue
 	case '"':
 		s.step = s.next
+		s.next = stateBeginKey
 		return scanContinue
 	}
 	return scanContinue
 }
 
+// stateInStringEsc is the state after reading `"\` during a quoted string.
+func stateInStringEsc(s *scanner, r rune) int {
+        switch r {
+        case 'b', 'f', 'n', 'r', 't', '\\', '/', '"':
+                s.step = stateInString
+                return scanContinue
+        }
+        return scanError
+}
+
 func stateEqual(s *scanner, r rune) int {
 	if '=' == r {
-		s.step = scanBeginValue
+		s.step = stateBeginValue
 		return scanEqual
 	}
 	return scanError
+}
+
+func stateInNumberOrUnit(s *scanner, r rune) int {
+	return -1
 }
