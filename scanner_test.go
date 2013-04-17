f9e505f69431200eb8d43949a15823d85f8935b1
Blake Mizerany <blake.mizerany@gmail.com>
2013-04-17 13:44:57 -0700

    better test

--- scanner_test.go
+++ scanner_test.go
@@ -19,74 +19,63 @@ func TestIsIdent(t *testing.T) {
 	}
 }
 
+type scannerTest struct {
+	r rune
+	w scannerState
+}
+
 func TestScanSimple(t *testing.T) {
-	data := `a=1 bar="foo\n" c=2h30s istrue isnull=`
-	want := []scannerState{
-		// a=1
-		scanBeginKey,
-		scanEqual,
-		scanBeginValue,
-
-		scanSkip,
-
-		// bar="foo\n"
-		scanBeginKey,
-		scanContinue,
-		scanContinue,
-		scanEqual,
-		scanBeginValue,
-		scanContinue,
-		scanContinue,
-		scanContinue,
-		scanContinue,
-		scanContinue,
-		scanContinue,
-
-		scanSkip,
-
-		// c=20h30s
-		scanBeginKey,
-		scanEqual,
-		scanBeginValue,
-		scanContinue,
-		scanContinue,
-		scanContinue,
-		scanContinue,
-
-		scanSkip,
-
-		// istrue
-		scanBeginKey,
-		scanContinue,
-		scanContinue,
-		scanContinue,
-		scanContinue,
-		scanContinue,
-
-		scanSkip,
-
-		// isnull=
-		scanBeginKey,
-		scanContinue,
-		scanContinue,
-		scanContinue,
-		scanContinue,
-		scanContinue,
-		scanEqual,
-
-		scanEnd,
+	tests := []scannerTest{
+		{'a', scanBeginKey},
+		{'=', scanEqual},
+		{'1', scanBeginValue},
+		{' ', scanSkip},
+		{' ', scanSkip},
+		{'b', scanBeginKey},
+		{'a', scanContinue},
+		{'r', scanContinue},
+		{'=', scanEqual},
+		{'"', scanBeginValue},
+		{'f', scanContinue},
+		{'o', scanContinue},
+		{'o', scanContinue},
+		{'\\', scanContinue},
+		{'n', scanContinue},
+		{'"', scanContinue},
+		{' ', scanSkip},
+		{'c', scanBeginKey},
+		{'=', scanEqual},
+		{'2', scanBeginValue},
+		{'h', scanContinue},
+		{'3', scanContinue},
+		{'0', scanContinue},
+		{'s', scanContinue},
+		{' ', scanSkip},
+		{'i', scanBeginKey},
+		{'s', scanContinue},
+		{'t', scanContinue},
+		{'r', scanContinue},
+		{'u', scanContinue},
+		{'e', scanContinue},
+		{' ', scanSkip},
+		{'i', scanBeginKey},
+		{'s', scanContinue},
+		{'n', scanContinue},
+		{'u', scanContinue},
+		{'l', scanContinue},
+		{'l', scanContinue},
+		{'=', scanEqual},
 	}
 
 	s := new(scanner)
 	s.reset()
-	for i, r := range data {
-		g := s.step(r)
-		t.Logf("%q: got %s", r, g)
-		if want[i] != g {
+	for i, test := range tests {
+		g := s.step(test.r)
+		if test.w != g {
 			if s.err != nil {
 				t.Error(s.err)
 			}
-			t.Fatalf("at %d: want %s, got %s", i, want[i], g)
+			t.Fatalf("at %d: want %s, got %s", i, test.w, g)
 		}
 	}
 }
