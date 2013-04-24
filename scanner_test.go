b6e7c65c74cdc7b7ed3b0e7c760acb580ea2d3e0
Blake Mizerany <blake.mizerany@gmail.com>
2013-04-23 22:13:06 -0700

    table tests

--- scanner_test.go
+++ scanner_test.go
@@ -6,31 +6,37 @@ import (
 )
 
 func TestScannerSimple(t *testing.T) {
-	data := []byte(`a=1 b="bar" ƒ=2h3s r="esc\t" d x=sf   `)
-
 	type T struct {
 		k string
 		v string
 	}
 
-	var want = []T{
-		{"a", "1"},
-		{"b", "bar"},
-		{"ƒ", "2h3s"},
-		{"r", "esc\t"},
-		{"d", ""},
-		{"x", "sf"},
+	tests := []struct {
+		data string
+		want []T
+	}{
+		{
+			`a=1 b="bar" ƒ=2h3s r="esc\t" d x=sf   `,
+			[]T{
+				{"a", "1"},
+				{"b", "bar"},
+				{"ƒ", "2h3s"},
+				{"r", "esc\t"},
+				{"d", ""},
+				{"x", "sf"},
+			},
+		},
 	}
 
-	var got []T
-
-	h := func(key, val []byte) error {
-		got = append(got, T{string(key), string(val)})
-		return nil
-	}
-	gotoScanner(data, HandlerFunc(h))
-
-	if !reflect.DeepEqual(want, got) {
-		t.Errorf("want %q, got %q", want, got)
+	for _, test := range tests {
+		var got []T
+		h := func(key, val []byte) error {
+			got = append(got, T{string(key), string(val)})
+			return nil
+		}
+		gotoScanner([]byte(test.data), HandlerFunc(h))
+		if !reflect.DeepEqual(test.want, got) {
+			t.Errorf("want %q, got %q", test.want, got)
+		}
 	}
 }
